6fd7d3270eb12dc766cac29cb0f645683744d3b4
Srustil <Srustilingaraju11@gmail.com>
2025-06-30 09:04:35 +0530

    Invalid LoginPage added

--- src/test/java/page/LoginPage.java
+++ src/test/java/page/LoginPage.java
@@ -1,14 +1,19 @@
 package page;
 
+
+import org.testng.Reporter;
+
 import com.microsoft.playwright.Page;
 
-public class LoginPage {
+public class LoginPage{
+	
 	private Page page;
 	
 	//declaration
 	private String userName="#input-username";
 	private String password="#input-password";
 	private String goButton=".btn-primary";
+	private String errMsg=".error";
 	
 	//initialization
 	public LoginPage(Page page)
@@ -31,5 +36,21 @@ public class LoginPage {
 	public void clickGoButton()
 	{
 		page.locator(goButton).click();
+	}
+	
+	
+	public boolean verifyErrMsgIsDisplayed()
+	{
+		try
+		{
+				page.locator(errMsg).waitFor();
+				String text = page.locator(errMsg).textContent();
+				Reporter.log(text,true);//home work--> print it in extent report
+				return true;
+		}
+		catch (Exception e) 
+		{
+				return false;
+		}
 	}
 }
